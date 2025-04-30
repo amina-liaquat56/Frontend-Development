e535527b9432ddc0964d2d772acb7703b7447945
Amina Liaquat <aminaliaquat41@gmail.com>
2025-04-30 10:10:21 +0500

    Create script.js

---
+++ JS /Class 01 /script.js
@@ -0,0 +1,47 @@
+// console.log("Amina Liaquat");
+// console.log(" MS Computer Sceince");
+// console.log("Javascript");
+
+     // Variables and Datatypes
+
+      // var , let , const
+
+// let fullname = "Tony Stark";
+// console.log(fullname)
+//  const age = 24;
+// console.log(age)
+//  let price = 99.99;
+// console.log(price)
+// let issfollows = true ;
+// console.log(isfollows)
+// let isfollows = false ;
+// console.log(isfollows)
+
+// console.log(typeof age);
+
+// const price1 = 50;
+// const price2 = 80;
+// let total = price1 + price2;
+
+
+        // Data Types
+
+// String ,Number , Bigint ,Boolean ,Undefined ,Null,Symbol, Object
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+
+// Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// Booleans
+// let x = true;
+// let y = false;
+
+// const a = null;
+// console.log(a)
+//  const y = undefined ;
+// console.log(y)
+
