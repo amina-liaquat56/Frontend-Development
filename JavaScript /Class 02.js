143f25e6f8d1eb8854258c245ce79f31272621d0
Amina Liaquat <aminaliaquat41@gmail.com>
2025-05-04 21:58:25 +0500

    Create Class 02.js

---
+++ JavaScript /Class 02.js
@@ -0,0 +1,36 @@
+   //  Comments in JS
+
+// This is single line comment
+
+/* This is a 
+  multiline comment */
+
+   // operators in JS
+
+//  Arithematic operator
+
+let a = 8;
+let b = 4;
+
+console.log("a =",a, "& b =" , b);
+console.log("a + b =",a + b);
+console.log("a - b =",a - b);
+console.log("a * b =",a * b);
+console.log("a / b =",a / b);
+console.log("a % b =",a % b);
+console.log("a ** b =",a ** b);
+
+   // Unary operators 
+let x = 5;
+// increment operator
+console.log("Increment: ++x =", ++x);
+
+// decrement operator
+console.log("Decrement: --x =", --x);
+
+ 
+
+
+
+
+
