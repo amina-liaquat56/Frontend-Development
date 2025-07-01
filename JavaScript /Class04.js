444e19fc2f856d79f07f8842956b3e3a44f06c4c
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-01 11:56:17 +0500

    Update Class04.js

--- JavaScript /Class04.js
+++ JavaScript /Class04.js
@@ -35,9 +35,12 @@ console.log(c, typeof c)
 
 let n = [2 ,3 , 4 ,5 ]
 
-n.pop()    // remove the last element
-
-n.push(9)    // add the element at end
+let r = n.pop() // pop returns the popped element
+console.log(n, r)
+let r = n.push(56) // push returns the new array length
+console.log(n, r)
+let r = n.shift()
+console.log(r, n) // Removes an element from the start of the array
 
 let r = n.shift()
 // console.log(r, n) // Removes an element from the start of the array
@@ -45,6 +48,36 @@ let r = n.shift()
 let r = n.unshift(72)
 console.log(r, n)
 console.log(r)
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+let num_even_more = [211, 212, 213, 214, 415, 416, 417, 418, 419]
+console.log(num.length)
+delete num[0]
+console.log(num.length)
+
+let newArray = num.concat(num_more, num_even_more)
+console.log(newArray)
+console.log(num, num_more)
+
+// sort method
+let compare = (a, b)=>{
+  return b - a
+}
+let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+num.sort(compare)
+num.reverse()
+console.log(num)
+
+// Splice and Slice
+let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+let deletedValues = num.splice(2, 4, 1021, 1022, 1023, 1024, 1025)
+console.log(num)
+console.log(deletedValues)
+
+let newNum = num.slice(3)
+let newNum = num.slice(3, 5)
+console.log(newNum)
 
 
 
