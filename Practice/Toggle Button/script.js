d9ed99048c8a7ae038475cb0f53ded543edc5084
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-11 21:48:07 +0500

    Delete Practice/Toggle Button/script.js

--- Practice/Toggle Button/script.js
+++
@@ -1,7 +0,0 @@
-toggleBtn.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
-  body.classList.toggle('light-mode');
-  toggleBtn.textContent = body.classList.contains('dark-mode') 
-    ? 'Toggle Light Mode' 
-    : 'Toggle Dark Mode';
-});
