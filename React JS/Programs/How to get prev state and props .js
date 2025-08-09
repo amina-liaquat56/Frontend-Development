3f8976d355414196d143f1bf03de9383341791f3
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-09 19:31:29 +0500

    Create How to get prev state and props .js

---
+++ React JS/Programs/How to get prev state and props .js
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from "react";
+export default function Counter() {
+  const [count, setCount] = useState(0);
+    const myRef = useRef();    
+    useEffect(() => {
+      myRef.val = count;
+    });
+    return (
+      <h1>
+        Now: {count}, before: {myRef.val}
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+      </h1>
+    );
+}
