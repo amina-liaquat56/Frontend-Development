44514637c9fb2c1b70974a94267ff500bd62ed8e
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-10 10:25:45 +0500

    Create Display previous state and  Current state.js

---
+++ React JS/Programs/Display previous state and  Current state.js
@@ -0,0 +1,15 @@
+export default function CounterDemo3() {
+    let [counter, updateCounter] = useState(0);
+    let prevValue = useRef(0);
+
+    let increment = () => {
+        updateCounter(counter + 1);
+    }
+    useEffect(() => {
+        prevValue.current = counter;
+    }, [counter]);
+    return <>
+        <p>Current value - {counter} , previous Value is-{prevValue.current}</p>
+        <button onClick={increment}>Increment</button>
+    </>
+}
